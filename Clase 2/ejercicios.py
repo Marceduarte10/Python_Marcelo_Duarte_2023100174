410222321c9a38c4f3f33a5f39f731fa6a3fa86e
unknown <nelsonmduarte.nmd@gmail.com>
2025-03-29 14:17:38 -0300

    finalizar clase 2

--- Clase 2/ejercicios.py
+++ Clase 2/ejercicios.py
@@ -2,15 +2,15 @@
 Crear un programa que pida dos numero y obtenga como resultado
 cual de ellos es par o si ambos lo son
 '''
-n1=int(input("Ingrese primer numero: "))
-n2=int(input("Ingrese segundo numero: "))
+numero1=int(input("Ingrese primer numero: "))
+numero2=int(input("Ingrese segundo numero: "))
 
-if((n1%2==0) and(n2%2==0)):
-    print("los numeros ",n1 , "y", n2, " son pares")
-elif ((n1%2==0) and (n2%2!=0)):
-    print("El numero ",n1, "es par, y el numero ", n2, "es impar")
-elif ((n1%2!=0) and (n2%2==0)):
-    print("El numero ",n2, "es par, y el numero ", n1, "es impar")
+if((numero1%2==0) and(numero2%2==0)):
+    print("los numeros ",numero1 , "y", numero2, " son pares")
+elif ((numero1%2==0) and (numero2%2!=0)):
+    print("El numero ",numero1, "es par, y el numero ", numero2, "es impar")
+elif ((numero1%2!=0) and (numero2%2==0)):
+    print("El numero ",numero2, "es par, y el numero ", numero1, "es impar")
 else:
-    print("los numeros ",n1 , "y", n2, " son impares")
+    print("los numeros ",numero1 , "y", numero2, " son impares")
 
