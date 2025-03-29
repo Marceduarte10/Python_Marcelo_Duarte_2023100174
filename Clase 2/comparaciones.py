2dd313a060da07f250079c6e85f86a43467f15ae
unknown <nelsonmduarte.nmd@gmail.com>
2025-03-29 20:30:53 -0300

    finalizar clase 2

--- Clase 2/comparaciones.py
+++ Clase 2/comparaciones.py
@@ -1,5 +1,17 @@
-
-
+'''
+Relaciones
+'''
 r= 100 ==100
 print(r)
 
+r=90 > 100
+print(r)
+
+r=90 < 100
+print(r)
+
+r=90 <= 100
+print(r)
+
+r=90 >= 100
+print(r)
--- Clase 2/ejercicios.py
+++ Clase 2/ejercicios.py
@@ -5,7 +5,7 @@ cual de ellos es par o si ambos lo son
 numero1=int(input("Ingrese primer numero: "))
 numero2=int(input("Ingrese segundo numero: "))
 
-if((numero1%2==0) and(numero2%2==0)):
+if((numero1%2==0) and (numero2%2==0)):
     print("los numeros ",numero1 , "y", numero2, " son pares")
 elif ((numero1%2==0) and (numero2%2!=0)):
     print("El numero ",numero1, "es par, y el numero ", numero2, "es impar")
@@ -13,4 +13,5 @@ elif ((numero1%2!=0) and (numero2%2==0)):
     print("El numero ",numero2, "es par, y el numero ", numero1, "es impar")
 else:
     print("los numeros ",numero1 , "y", numero2, " son impares")
+
 
