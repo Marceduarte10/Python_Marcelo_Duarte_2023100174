207b6d9e581a885f8a6f2c3b3a0f00c45fc15d28
Marcelo <nelsonmduarte.nmd@gmail.com>
2025-05-17 18:00:22 -0300

    Clase 6 y 7

--- Clase 5/mi_api/app.py/login.py
+++ Clase 5/mi_api/app.py/login.py
@@ -7,39 +7,40 @@ def llamarServicioSet():
     user = request.json.get('user')
     password = request.json.get('password')
 
-    codRes, menRes, accion = inicializarVariables(user, password) 
+    
+    codRes, menRes, accion = inicializarVariables(user, password)
 
     salida = {
-        'codRes' : codRes,
-        'menRes' : menRes,
-        'usuario' : user,
-        'accion' : accion
+        'codRes': codRes,
+        'menRes': menRes,
+        'usuario': user,
+        'accion': accion
     }
-    return jsonify (salida)
+    return jsonify(salida)
 
-    def inicializarVariables(user, password):
-        userLocal = "mduarte"
-        passLocal = "unida123"
-        codRes = 'SIN_ERROR'
-        menRes = 'OK'
+def inicializarVariables(user, password):
+    userLocal = "dcaballero"
+    passLocal = "unida123"
+    codRes = 'SIN_ERROR'
+    menRes = 'OK'
+    
+    try:
+        print("Verificar login")
+        print("Local",password,"Remote:", password,"UserLocal:",userLocal,"UserRemote",user)
+        if password == passLocal and user == userLocal:
+            print("Usuario y contraseña OK")
+            accion = "Success"
+        else:
+            print("Usuario o contraseña incorrecta")
+            accion = "Usuario o contraseña incorrecta"
+            codRes = 'ERROR'
+            menRes = 'Credenciales o usuario incorrectas'
 
-        try: 
-            print("Verificar login")
-            if password == passLocal and user == userLocal:
-                print("Usuario y contrasela OK")
-                accion = "Success"
-            else:
-                print("Usuario o Contraseña incorrecta")
-                accion = "Usuario o contraseña incorrecta"
-                codRes = 'ERROR'
-                menRes = 'Credenciales del usuario incorrectas'
 
+    except Exception as e:
+        print("ERROR", str(e))
+        codRes = 'ERROR'
+        menRes = 'Msg: ' + str(e)
+        accion = "Error interno"
 
-except Exception as e:
-    print("ERROR", str(e))
-    codRes = 'ERROR'
-    menRes = 'Msg: ' + str(e)
-    accion = "Eror interno"
-
-return codRes, menRes, accion
- 
+    return codRes, menRes, accion
--- Clase 5/mi_api/app.py/main.py
+++ Clase 5/mi_api/app.py/main.py
@@ -1,37 +1,18 @@
 #!/usr/bin/env python3
 from flask import Flask
 from login import login
+from login2 import login2
+
 
 app = Flask(__name__)
 
-#servicios rest
+# Registrar el blueprint de login
 app.register_blueprint(login)
+app.register_blueprint(login2)
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hola mundo'
+    return 'Hola Unida'
 
 if __name__ == "__main__":
-
-    app.run(host = '0.0.0.0', debug = True, port = 8081)
-    app.run(debug = True)
-    
-from flask import Blueprint, request, jsonify
-
-login = Blueprint('login', __name__)
-
-@login.route('/login', methods=['POST'])
-def llamarServicioSet():
-    user = request.json.get('user')
-    password = request.json.get ('password')
-
-    codRes, menRes, accion = inicializarVariables(user, password)
-
-    salida = {
-        'codRes' : codRes,
-        'menRes' : menRes,
-        'usuario' : user, 
-        'accion' : accion
-    }
-
-    return jsonify(salida)
+    app.run(host='0.0.0.0', port=5001, debug=True)
